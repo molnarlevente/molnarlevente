9b7d978d244696644a4b3cd60e4b428132f17cf9
Levente <molnarlevi2016@gmaio>
2022-02-06 13:43:53 +0100

    #1 több sor #2 több sor

--- 01oraiAnyag.py
+++ 01oraiAnyag.py
@@ -23,8 +23,58 @@ print()
 a = 'válozóba tárolt string'
 print(a)
 
- x  15
- y  3.5
- z
+x = 15
+y = 3.5
+z = 'szöveg'
 
- print(a,x,y,z)
+print(a,x,y,z)
+
+
+# változó értékének a növelése
+b = 1
+print(b)
+b = b + 1 # a b változóban tárolz adat értékének növelése eggyel
+print(b)
+b += 1 # a b változóban tárolt adar értékének növelése eggyel
+print(b)
+# b=+1 #ha ezt így írod akkor olyan mintha beállítanád neki a b = 1 azaz b értéke egy lesz
+
+#változó tíusának a megváltoztatása
+print()       #sortörés
+ido = 'süt a nap' #str
+print(ido)
+ido= 'felhős'  #érték megváltoztatása
+print(ido)
+ido = 1990     # típus változtatása
+print(ido)
+
+#kiértékelés len beépített függvény használat
+sajatnevem = 'Molnár Levente'
+print(len(sajatnevem))
+
+#összeadás
+print()
+d=10
+c=5
+e=c+d
+print(e)       #változóba eltárolva
+print(c+d)     #kiiratás közben
+
+#szöveg hozzáadása a kiíratáshoz
+print('\nA két szám összege: '+str(e))     #típuskonverzió str(e) (kasztolás)
+#vagy
+print('\nA két szám összege: ',e)          #int típusként való kiíratás
+
+print(f'\nA két szám összege: {e}')         #sztring interpoláció (a szövegbe bele tudom illeszteni  aváltozómat akár
+                                              # többet is!)
+
+#típukonverziós függvény ami nem egyenlő a változó típusával!!!
+#ezt a terminál ablakban futatom!!!
+int(25)
+25
+float(25)
+25.0
+str(25)
+'25'
+
+
