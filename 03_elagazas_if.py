f48a8d8c4a6a5ab576704eb22b1bb1406d61e9c0
Levente <molnarlevi2016@gmaio>
2022-02-23 17:38:55 +0100

    #4 if and elagazas

---
+++ 03_elagazas_if.py
@@ -0,0 +1,54 @@
+#booleam típusok
+#>>> type(True)                      <class 'bool'>
+#>>> type(False)                     #<class 'bool'>
+
+#összehasonlító operátorok:
+    # gyenlő                 a == b
+    # Nem egyenlő            a != b (!!negátolás!! = tagadod)
+    # kisebb mint:           a < b
+    # nagyobb mint:          a > b
+    # kisebb vagy egyenlő:   a <= b
+    # nagyobb vagy egyenlő:  b >= b
+
+#logikai operátorok:
+    #És =>       and
+    #vagy =>     or
+    #tagadás =>  not
+
+#>>> n = 9
+#>>> n%2==0 or n%3==0
+#True
+#>>> n = 9
+#xx>>> n%2==0 and n%3==0
+#False
+#>>> not(x<n)
+#False
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
