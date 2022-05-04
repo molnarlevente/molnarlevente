db735b4da953b4a84c8f10c52e2815b9d8eabc5a
Levente <molnarlevi2016@gmaio>
2022-05-04 17:07:50 +0200

    #9 lista

--- 09_Lista.py
+++ 09_Lista.py
@@ -16,8 +16,8 @@ listaalistaban=[1,3,4,[szamok]]
 # #minden elem kiratáse
 # #ciklussal:
 #
-# for elemek in szamok:
-#     print(elemek)
+for elemek in szamok:
+    print(elemek)
 #
 # print(listaalistaban[4])        # a beslo lista a 4.index
 #     # ROSSZ      print(listaalistaban[12321312]) # ez nem szeletelés, nem lehet kiiratni a nem létező indexet, a program hibára fut
@@ -28,50 +28,137 @@ listaalistaban=[1,3,4,[szamok]]
 #
 #
 #
-# #keresés a listában
-# db = 0
+#keresés a listában
+db = 0
+
+print(32 in szamok)
+
+if 32 in szamok:
+    print(f"van találat")           # for-al    VIZSGÁLAT
+    db+=1
+else:
+    print(f"nincs")
+
 #
-# print(32 in szamok)
+# #lista műveletek :
 #
-# if 32 in szamok:
-#     print(f"van találat")           # for-al    VIZSGÁLAT
-#     db+=1
-# else:
-#     print(f"nincs")
+# osszeadas= szamok + szamok
+# print(osszeadas)
+# print(*osszeadas)
 #
+# # szorzás         *
+# szorzas=szamok*2
+# print(szorzas)
+#
+# # szeletelés  [ x : y ]
+# print(szamok[1:4])
+# print(szamok[:4])
+# print(szamok[4:])
+#
+# # elemek módosítása
+# print(*szoveg)
+# szoveg[2]="egésznap"
+# print(*szoveg)
+#
+# szoveg[1:1]="FaliÓra"
+# print(szoveg)
+#
+# #elem törlése
+# print(szamok)
+# del szamok[2]
+# print(szamok)
+#
+# #szövegben tólig kitörlés
+#
+# print(szoveg)
+# del szoveg[1:8]
+# print(szoveg)
+#
+# #hozzá fűz az utolsó lista elem után
+# szoveg.append("valami")
+# print(szoveg)
 
-# lista műveletek :
+# objektumok és hivatkozások
+    # sztring esetén: ugyan arra az objektumra mutat az alábbi módon bizonyítjuk
+a = "valami"
+b = "valami"
 
-osszeadas= szamok + szoveg
-print(osszeadas)
-print(*osszeadas)
+print(a == b)
 
-# szorzás         *
-szorzas=szamok*2
-print(szorzas)
+print(a in b)
+print(50*"!")
+#viszont a lista esetében ugyan KÜLÖN objektumra mutat az alábbi módon bizonyítjuk
+list1 = [1,2,3,4,]
+list2 = [1,2,3,4,]
 
-# szeletelés  [ x : y ]
-print(szamok[1:4])
-print(szamok[:4])
-print(szamok[4:])
+print(list1==list2)     #==> true
 
-# elemek módosítása
-print(*szoveg)
-szoveg[2]="egésznap"
-print(*szoveg)
+print(list1 in list2)   #==> false
 
-szoveg[1:1]="FaliÓra"
-print(szoveg)
+                                                                        #tk 150.oldal (fedőnevek)
 
-#elem törlése
-print(szamok)
-del szamok[2]
-print(szamok)
+ # klónozás
+a = ["egy","kettő"]
+b = a[:]
 
-#szövegben tólig kitörlés
+print(*b)
 
-print(szoveg)
-del szoveg[1:8]
-print(szoveg)
+a.append("három")
+
+print(*a)
+print(*b)
 
 
+# lista és for-ciklus használata
+
+#kész. egy listát majd az elemeket szorozzuk meg kettővel
+
+szorzat= [1,2,3,4,5,6]
+
+for i in szorzat:
+    print(f"az eredeti szám {i} majd ezt megszorzom 2-vel és ezt fogom kapni {i*2}")
+
+for (i, ertek) in enumerate(szorzat):                                                        # indexelve írjuk ki   enumerate !!!!!!!!!!!!!!!!!
+    print(f"a sorozat indexe:{i}, az indexen a {ertek*2} szám *2 értéke szerepel ")
+
+    #   sztringbe
+    print("-"*50)
+szoveg= ["alma", "virág", "eper","kutya","ember"]
+
+for (i, ertek) in enumerate(szoveg):
+    print(i, ertek)
+
+    print("_" * 50)
+                #       lista metódusok  (beép.függvények)
+
+mlista = []
+print(mlista)
+
+mlista.append(85)
+mlista.append(65)
+mlista.append(4)
+mlista.append(418)          # mindeig az utolsó poz.-ra
+mlista.append(12)
+print(mlista)
+
+mlista.insert(2, 45)        # beszúrás az adott helyre de nem cseréli ki, erébb rólja, !!!!!!!pozicíóra!!!!!!!
+print(mlista)
+
+print(50*"-")
+sorbaredezve= sorted(mlista)
+print(sorbaredezve)         # növ.sorrend az eredeti lista nem módosul!!!!!!!!!!
+print(mlista)
+print(50*"-")
+
+mlista.reverse()            # elemek megfordítás, de az eredeti lista módosul!!!!!!!!!!
+print(mlista)
+
+    #   rendezzükk a listát növekvő sorrendbe
+
+mlista.sort()               # sorba rendez, de az eredeti lista módosul!!!!!!!!!!
+print(mlista)
+
+    #   csökk. sorrend, de az eredeti lista nem módosul!!!!!!!!!!
+print(50*"-")
+print(sorted(mlista, reverse=True))
+print(50*"-")
