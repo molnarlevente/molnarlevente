6ac72602ad0cecc50a6b82c3c2bd8154f4ae16b4
Levente <molnarlevi2016@gmaio>
2022-07-20 11:17:21 +0200

    #15_erettsegis_feladatok_befejezése_1

--- 15_erettsegi_feladatok.py
+++ 15_erettsegi_feladatok.py
@@ -1,66 +1,157 @@
-print("1.feladat")
+# lista=[]
+# with open("erettsegi_feladtaok_allomany/lista-1.txt", encoding="UTF8") as f:
+#     for sor in f:
+#         adat = sor.strip().split()
+#         if len(adat) == 5:
+#             datum=adat[0]
+#             sorozat=adat[1]
+#             resze=adat[2]
+#             hossza=adat[3]
+#             latta = adat[4]
+#             adatok=[datum,sorozat,resze,hossza,latta]
+#             lista.append(adatok)
+#
+# for elem in lista:
+#     print(elem[0])
+# print(adat)
+#
+# print("2.feladat")
+#
+# db=0
+#
+# for adatok in adat:
+#     if "NI" not in adat[0]: #datum
+#         db+=1
+#
+# print(f"A listában {db} db vetítési dátummal rendelkező epizód van.")
+#
+#
+# print("3.feladat")
+#
+# szamlalo=0
+#
+# for adatok in adat:
+#     if adatok[latta]==True:
+#         szamlalo+=1
+#
+# szazalek_szam=szamlalo/len(adat) *100
+# print(f"A listában lévő epizódok {szazalek_szam}%-át látta.")
+#
+# print("4.feladat")
+# osszes_ido=0
+#
+# for adatok in adat:
+#     if adatok[latta]==True:
+#         osszes_ido += hossza
+#
+# nap=osszes_ido // (24 ** 60)
+# ora=osszes_ido % (24 * 60) // 60
+# perc=osszes_ido % 60
+#
+# print(f"Sorozatnézéssel {nap} napot {ora} órát és {perc} percet töltött.")
+#
+# print("5.feladat")
+#
+# datum=input(f"Adjon meg egy dátumot! Datum= ")
+#
+# for adatok in adat:
+#     if adatok[latta] <= datum and latta==False:
+#         print(resze  )                                      #!!!!!!
+#
+#
+# print("6.feladat")
+#
 
-adat=[]
-with open ("erettsegi_feladtaok_allomany/lista-1.txt", encoding="UTF8") as f:
-    for sor in f:
-        adat.append(sor.strip())
-        if len(adat) == 5:
-            datum=adat[0]
-            sorozat=adat[1]
-            resze=adat[2]
-            hossza=int(adat[3])
-            if adat[4] == 1:
-                latta=True
-            else:
-                latta=False
-            "adat.append(datum,sorozat,resze,hossza,latta)"
-print(adat)
+#while True !!!!!!!!!
 
-print("2.feladat")
+print("ceges autok" and "*"*100)
 
+                                                                    #_______________________________________________________________________
+autok_lista = []
+with open("erettsegi_feladtaok_allomany/info_feladat/2020_okt/autok.txt", encoding="UTF-8") as f:
+    for adat in f:
+        sor = adat.strip().split()
+
+        nap=int(sor[0])
+        ido=sor[1]
+        rendszam=sor[2]
+        azonosito=int(sor[3])
+        km=int(sor[4])
+        # if sor[5] == 0:
+        #     ki = "ki"
+        # else:
+        #     be = "be"
+        ki_be_hajtas=int(sor[5])
+#
+        adatok=[nap,ido,rendszam,azonosito,km,ki_be_hajtas]
+        autok_lista.append(adatok)
+
+# for elem in autok_lista:
+#     print(rendszam)
+
+print("2. feladat")
+
+for elem in autok_lista:
+    if elem[5] == 0:
+        nap = elem[0]
+        rendszam = elem[2]   # az utolso marad benne amit eltárol !!!!
+
+print(f"{nap} nap rendszám: {rendszam}")
+
+print("3. feladat")
+
+#bekeret_nap=input("Nap: ")
+bekeret_nap = 4
+print(f"Forgalom a(z) {bekeret_nap} napon:")
+for elem in autok_lista:
+
+    # if elem[5] == 0:
+    #     ki = "ki"             # nem kell, felesleges
+    # else:
+    #     be = "be"
+
+
+    if elem[0] == bekeret_nap:
+        if elem[5] == 0:
+            print(f"{elem[1]} {elem[2]} {elem[3]} ki")
+        else:
+            print(f"{elem[1]} {elem[2]} {elem[3]} be")
+
+
+print("4. feladat")
 db=0
-
-for adatok in adat:
-    if "NI" not in adat[0]: #datum
+db2=0
+for elem in autok_lista:
+    if ki_be_hajtas==0:
         db+=1
+    else:
+        db2+=1
 
-print(f"A listában {db} db vetítési dátummal rendelkező epizód van.")
+print(f"A hónap végén {db-db2} autót nem hoztak vissza.")
 
+print("5. feladat")
+rendszamoklista=[]
+for elem in autok_lista:
+    if sor[2]  not in rendszamoklista:
+        sor[2] += rendszamoklista
 
-print("3.feladat")
+rendszamoklista = sorted(rendszamoklista)                           ### sorted
+#print(rendszamoklista)
 
-szamlalo=0
+for adat_rendszam in rendszamoklista:
+    megtett_km_ek = []
+    for elem in autok_lista:
+        if adat_rendszam == elem[2]:
+            megtett_km_ek.append(elem[4])
 
-for adatok in adat:
-    if adatok[latta]==True:
-        szamlalo+=1
+    kezdo_km = megtett_km_ek[0]
+    utolso_km = megtett_km_ek[-1]
+    megtettkm= utolso_km - kezdo_km
 
-szazalek_szam=szamlalo/len(adat) *100
-print(f"A listában lévő epizódok {szazalek_szam}%-át látta.")
-
-print("4.feladat")
-osszes_ido=0
-
-for adatok in adat:
-    if adatok[latta]==True:
-        osszes_ido += hossza
-
-nap=osszes_ido // (24 ** 60)
-ora=osszes_ido % (24 * 60) // 60
-perc=osszes_ido % 60
-
-print(f"Sorozatnézéssel {nap} napot {ora} órát és {perc} percet töltött.")
-
-print("5.feladat")
-
-datum=input(f"Adjon meg egy dátumot! Datum= ")
-
-for adatok in adat:
-    if adatok[latta] <= datum and latta==False:
-        print(resze  )                                      #!!!!!!
+    print(f"{adat_rendszam} {megtettkm} km")
 
 
-print("6.feladat")
+
 
 
 
