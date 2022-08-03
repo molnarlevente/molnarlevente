ba1e77b3c0093a0471b4c2dcbb62e2ce453f2634
Levente <molnarlevi2016@gmaio>
2022-08-03 11:47:55 +0200

    #17kiiratasok__bekérés

--- 17_kiiratas_allomanyba.py
+++ 17_kiiratas_allomanyba.py
@@ -1,12 +1,13 @@
         #           kiírás fájlba!
-
-
-
-
 with open("kiiratasok/elsokiiras.txt", "w", encoding="UTF-8") as f:
     f.writelines("Szia Levi!")          #soronként ír ki állomány
 
+        #           változó értékének kiiratása
 szoveg =    "Szia Levi!     Most már változóból! "
-
 with open("kiiratasok/masodikkiiras.txt", "w", encoding="UTF-8") as f:
     f.writelines(szoveg)
+
+        #           bekérés
+valasz = input("MI legyen a fájl neve?: ")
+with open(f"kiiratasok/{valasz}.txt", "w", encoding="UTF-8") as f:
+    f.writelines("felhasználó álltal megadott fájlnév!")
