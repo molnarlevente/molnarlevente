5854d3ca4c86e9b82c6e7190aa7a12167ea2aa45
Levente <molnarlevi2016@gmaio>
2022-08-03 11:37:26 +0200

    #17kiiratasok__valtozoval

--- 17_kiiratas_allomanyba.py
+++ 17_kiiratas_allomanyba.py
@@ -5,3 +5,8 @@
 
 with open("kiiratasok/elsokiiras.txt", "w", encoding="UTF-8") as f:
     f.writelines("Szia Levi!")          #soronként ír ki állomány
+
+szoveg =    "Szia Levi!     Most már változóból! "
+
+with open("kiiratasok/masodikkiiras.txt", "w", encoding="UTF-8") as f:
+    f.writelines(szoveg)
